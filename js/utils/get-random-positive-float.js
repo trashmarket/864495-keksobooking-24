65b9d3e72ea0618b8482091256c637a42095d6f7
Nikita <prey122@gmail.com>
2021-10-06 20:12:55 +0300

    import-export

--- js/utils/get-random-positive-float.js
+++ js/utils/get-random-positive-float.js
@@ -1,6 +1,6 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
+export {getRandomPositiveFloat};
 function getRandomPositiveFloat (a, b, digits = 1) {
   // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
   // реализуем поддержку передачи минимального и максимального значения в любом порядке,
