85a39df3c750d1c554968c5b1a81c5c5e0ce853e
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-11-16 16:07:06 +0300

    improve sync on capacity & room count

--- js/form-utils/ensure-available-capacilty.js
+++ js/form-utils/ensure-available-capacilty.js
@@ -1,12 +1,12 @@
 const roomCountCapacitySettings = {
-  1:[1],
-  2:[1,2],
-  3:[1,2,3],
-  100: [0],
+  1:['1'],
+  2:['1','2'],
+  3:['1','2','3'],
+  100: ['0'],
 };
 
 const ensureEnabled = (capacityArray, capacityOptions)=>{
-  capacityOptions.forEach((option)=>option.disabled = capacityArray.some((c)=>c === option.value));
+  capacityOptions.forEach((option)=>option.disabled = capacityArray.every((c)=>c !== option.value));
 };
 
 export const ensureCapacityAvailable = (roomCountInput, capacityInput)=>{
