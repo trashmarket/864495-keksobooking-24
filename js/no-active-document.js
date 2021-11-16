da733e902478d66f824fa1e58c3e7df81accf6eb
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-11-16 14:32:04 +0300

    set form state in general

--- js/no-active-document.js
+++ js/no-active-document.js
@@ -1,3 +1,20 @@
+const settings = {
+  disabled: {
+    action: 'add',
+    flag: true,
+  },
+  enabled: {
+    action: 'remove',
+    flag: false,
+  },
+};
+const setFormState = (form, state) => {
+  form.classList[state.action]('ad-form--disabled');
+  [...form.elements].forEach((item) => {
+    item.disabled = state.flag;
+  });
+};
+
 const shutDownDocument = function (form, formChildren, mapForm, mapFormChildren) {
   form.classList.add('ad-form--disabled');
   [...form.elements].forEach((item) => {
@@ -10,4 +27,6 @@ const shutDownDocument = function (form, formChildren, mapForm, mapFormChildren)
   });
 };
 
-export { shutDownDocument };
+export {
+  shutDownDocument
+};
